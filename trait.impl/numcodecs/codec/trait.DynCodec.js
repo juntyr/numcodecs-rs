69c13f4873674e50ed1ba9b6166e20132b5399bf
juntyr <juntyr@users.noreply.github.com>
2024-08-15 07:20:34 +0000

    deploy: 3472d21bb64c70c0e80dd82280e45d34c1544c6b

--- trait.impl/numcodecs/codec/trait.DynCodec.js
+++ trait.impl/numcodecs/codec/trait.DynCodec.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "numcodecs":[],
-"numcodecs_python":[["impl DynCodec for <a class=\"struct\" href=\"numcodecs_python/struct.PyCodecAdapter.html\" title=\"struct numcodecs_python::PyCodecAdapter\">PyCodecAdapter</a>"]]
+"numcodecs_python":[["impl <a class=\"trait\" href=\"https://juntyr.github.io/numcodecs-rs/numcodecs/codec/trait.DynCodec.html\" title=\"trait numcodecs::codec::DynCodec\">DynCodec</a> for <a class=\"struct\" href=\"numcodecs_python/struct.PyCodecAdapter.html\" title=\"struct numcodecs_python::PyCodecAdapter\">PyCodecAdapter</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
