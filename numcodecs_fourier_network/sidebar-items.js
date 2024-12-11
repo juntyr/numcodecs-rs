bc23d9270da015effd97584eeca968fc1c9ce697
juntyr <juntyr@users.noreply.github.com>
2024-12-11 07:55:58 +0000

    deploy: 9acf23c03463d4f228e1fe39eb3facaef48a790e

---
+++ numcodecs_fourier_network/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["FourierNetworkCodecError"],"fn":["decode_into","encode"],"struct":["BlockRecord","BlockRecordItem","FourierNetworkCodec","ModelExtraRecord","ModelExtraRecordItem","ModelRecord","ModelRecordItem","NeuralNetworkError","Positive"],"trait":["FloatExt"]};
---
+++ trait.impl/burn_core/record/base/trait.Record.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["numcodecs_fourier_network",[["impl&lt;B: Backend&gt; Record&lt;B&gt; for <a class=\"struct\" href=\"numcodecs_fourier_network/struct.BlockRecord.html\" title=\"struct numcodecs_fourier_network::BlockRecord\">BlockRecord</a>&lt;B&gt;"],["impl&lt;B: Backend&gt; Record&lt;B&gt; for <a class=\"struct\" href=\"numcodecs_fourier_network/struct.ModelExtraRecord.html\" title=\"struct numcodecs_fourier_network::ModelExtraRecord\">ModelExtraRecord</a>&lt;B&gt;"],["impl&lt;B: Backend&gt; Record&lt;B&gt; for <a class=\"struct\" href=\"numcodecs_fourier_network/struct.ModelRecord.html\" title=\"struct numcodecs_fourier_network::ModelRecord\">ModelRecord</a>&lt;B&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[667]}
