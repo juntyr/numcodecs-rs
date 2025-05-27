e1138f5f30527fa6824d32e685e67c133afeea6b
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-05-27 07:07:07 +0000

    Hotglue for sperr and sz3

--- crates/numcodecs-wasm-builder/buildenv/include.hpp
+++ crates/numcodecs-wasm-builder/buildenv/include.hpp
@@ -4,63 +4,101 @@
 #include <cstdlib>
 #include <new>
 
-extern "C" inline void __cxa_pure_virtual() { std::abort(); }
-extern "C" inline void* __cxa_allocate_exception(size_t) throw() { std::abort(); }
-extern "C" inline void __cxa_throw() { std::abort(); }
-extern "C" inline unsigned int __cxa_uncaught_exceptions() { return 0; }
+extern "C" inline void __cxa_pure_virtual()
+{
+    std::abort();
+}
 
-inline void* operator new(std::size_t n)
+extern "C" inline void *__cxa_allocate_exception(size_t) throw()
+{
+    std::abort();
+}
+
+extern "C" inline void __cxa_throw()
+{
+    std::abort();
+}
+
+extern "C" unsigned int __attribute__((weak)) __cxa_uncaught_exceptions();
+extern "C" unsigned int __cxa_uncaught_exceptions()
+{
+    return 0;
+}
+
+void *__attribute__((weak)) operator new(std::size_t n)
+#if __cplusplus < 201703L
+    throw(std::bad_alloc)
+#endif
+        ;
+void *operator new(std::size_t n)
 #if __cplusplus < 201703L
     throw(std::bad_alloc)
 #endif
 {
-    void* ret = std::malloc(n);
-    if (ret == nullptr) {
+    void *ret = std::malloc(n);
+    if (ret == nullptr)
+    {
         std::abort();
     }
     return ret;
 }
 
-inline void* operator new[](std::size_t n)
+void *__attribute__((weak)) operator new[](std::size_t n)
+#if __cplusplus < 201703L
+    throw(std::bad_alloc)
+#endif
+        ;
+void *operator new[](std::size_t n)
 #if __cplusplus < 201703L
     throw(std::bad_alloc)
 #endif
 {
-    void* ret = std::malloc(n);
-    if (ret == nullptr) {
+    void *ret = std::malloc(n);
+    if (ret == nullptr)
+    {
         std::abort();
     }
     return ret;
 }
 
-inline void* operator new(std::size_t n, std::align_val_t a)
+void *__attribute__((weak)) operator new(std::size_t n, std::align_val_t a)
+#if __cplusplus < 201703L
+    throw(std::bad_alloc)
+#endif
+        ;
+void *operator new(std::size_t n, std::align_val_t a)
 #if __cplusplus < 201703L
     throw(std::bad_alloc)
 #endif
 {
-    void* ret = aligned_alloc(static_cast<std::size_t>(a), n);
-    if (ret == nullptr) {
+    void *ret = aligned_alloc(static_cast<std::size_t>(a), n);
+    if (ret == nullptr)
+    {
         std::abort();
     }
     return ret;
 }
 
-inline void operator delete(void* p) noexcept
+void __attribute__((weak)) operator delete(void *p) noexcept;
+void operator delete(void *p) noexcept
 {
     std::free(p);
 }
 
-inline void operator delete[](void* p) noexcept
+void __attribute__((weak)) operator delete[](void *p) noexcept;
+void operator delete[](void *p) noexcept
 {
     std::free(p);
 }
 
-inline void operator delete(void* p, std::align_val_t a) noexcept
+void __attribute__((weak)) operator delete(void *p, std::align_val_t a) noexcept;
+void operator delete(void *p, std::align_val_t a) noexcept
 {
     std::free(p);
 }
 
-inline void operator delete(void* p, std::size_t sz) noexcept
+void __attribute__((weak)) operator delete(void *p, std::size_t sz) noexcept;
+void operator delete(void *p, std::size_t sz) noexcept
 {
     std::free(p);
 }
