2f411db7880c02f290784e945b60107d94b23450
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-02-12 19:50:09 +0200

    cleanup

--- crates/numcodecs-wasm-builder/buildenv/include.hpp
+++ crates/numcodecs-wasm-builder/buildenv/include.hpp
@@ -3,17 +3,11 @@
 #define _CODECS_BUILD_CPP_INCLUDE_HPP
 
 #include <cstdlib>
-// #include <typeinfo>
 
 extern "C" inline void *__cxa_allocate_exception(size_t) throw()
 {
     std::abort();
 }
-
-// extern "C" inline void __cxa_throw(void *thrown_exception, std::type_info *tinfo, void *(*dest)(void *))
-// {
-//     std::abort();
-// }
 
 #endif // _CODECS_BUILD_CPP_INCLUDE_HPP
 #endif // __ASSEMBLER__
