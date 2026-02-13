a0a6e701bd9a0582f77ff54f1fdb1bc5ebe0788d
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2026-02-13 13:48:47 +0200

    Support wasm exceptions in the numcodecs-wasm buildenv (#49)
    * Enable out wasm exceptions in the buildenv
    
    * wasm-opt --enable-reference-types --enable-exception-handling
    
    * Remove __cxa_allocate_exception stub
    
    * Use -O3 in wasm-opt
    
    * Upgrade exceptional codecs

--- crates/numcodecs-wasm-builder/buildenv/include.hpp
+++ crates/numcodecs-wasm-builder/buildenv/include.hpp
@@ -2,12 +2,5 @@
 #ifndef _CODECS_BUILD_CPP_INCLUDE_HPP
 #define _CODECS_BUILD_CPP_INCLUDE_HPP
 
-#include <cstdlib>
-
-extern "C" inline void *__cxa_allocate_exception(size_t) throw()
-{
-    std::abort();
-}
-
 #endif // _CODECS_BUILD_CPP_INCLUDE_HPP
 #endif // __ASSEMBLER__
